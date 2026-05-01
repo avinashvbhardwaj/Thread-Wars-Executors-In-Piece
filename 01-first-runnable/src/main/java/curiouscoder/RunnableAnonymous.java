3cd9583a45deed3eba248ad2f90cfd89a956bf35
Avinash Bhardwaj <avinashvbhardwaj@gmail.com>
2026-05-02 01:03:20 +0530

    It is lembda or anonymous

--- 01-first-runnable/src/main/java/curiouscoder/RunnableAnonymous.java
+++ 01-first-runnable/src/main/java/curiouscoder/RunnableAnonymous.java
@@ -5,7 +5,6 @@ import java.util.stream.IntStream;
 public class RunnableAnonymous {
 
     static void main() {
-        System.out.println("starting main thread.....");
 
         new Thread(() -> {
             IntStream.range(1, 10).boxed().forEach((i) -> System.out.println("Runner2 : " + i));
@@ -13,8 +12,6 @@ public class RunnableAnonymous {
         new Thread(() -> {
             IntStream.range(1, 10).boxed().forEach((i) -> System.out.println("Runner1 : " + i));
         }).start();
-
-        System.out.println("ending main thread.....");
 
     }
 
