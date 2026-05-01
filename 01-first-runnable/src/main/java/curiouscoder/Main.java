af16ab67a1bf42f8af6f5d3e8fb60248d0779df0
Avinash Bhardwaj <avinashvbhardwaj@gmail.com>
2026-05-01 22:59:52 +0530

    it starts with runnable

---
+++ 01-first-runnable/src/main/java/curiouscoder/Main.java
@@ -0,0 +1,35 @@
+package curiouscoder;
+
+import java.util.stream.IntStream;
+
+class Runnable1 implements Runnable {
+
+    @Override
+    public void run() {
+        IntStream.range(1, 10).boxed().forEach((i) ->System.out.println("Runner1 : " + i));
+    }
+}
+
+
+class Runnable2 implements Runnable {
+
+    @Override
+    public void run() {
+        IntStream.range(1, 10).boxed().forEach((i) ->System.out.println("Runner2 : " + i));
+    }
+}
+
+public class Main {
+
+    static void main() {
+        System.out.println("starting main thread.....");
+
+        System.out.println("======================================");
+        new Thread(new Runnable1()).start();
+        new Thread(new Runnable2()).start();
+
+        System.out.println("ending main thread.....");
+
+    }
+
+}
